8e9edb8bdd2401cfb75c0d0b05fabba65b4ca5e0
Iwamoto Yoshiki <hosomi6930@icloud.com>
2020-05-13 11:46:47 +0900

    print("あいうえお")を追記しました

---
+++ GitTestProject/ViewController.swift
@@ -0,0 +1,22 @@
+//
+//  ViewController.swift
+//  GitTestProject
+//
+//  Created by 岩元喜輝 on 2020/05/13.
+//  Copyright © 2020 Yoshiki Iwamoto. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        print("あいうえお")
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
